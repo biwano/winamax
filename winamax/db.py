10e7459157044ea32771f76a0af4da55e5ec7304
Bruno Ilponse <bruno.ilponse@gmail.com>
2021-03-20 18:30:45 +0100

    New database leech method

--- winamax/db.py
+++ winamax/db.py
@@ -3,6 +3,9 @@ from sqlalchemy import create_engine, Table, Index, Column, Numeric, BigInteger,
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from contextlib import contextmanager
+from sqlalchemy.orm.exc import NoResultFound
+from datetime import datetime
+import json
 
 Base = declarative_base()
 
@@ -12,6 +15,21 @@ class History(Base):
    outcome_id = Column(Integer, primary_key = True)
    time = Column(BigInteger, primary_key = True)
    data = Column(String)
+
+class Match(Base):
+   __tablename__ = 'match'
+   
+   match_id = Column(Integer, primary_key = True)
+   sport_id = Column(Integer)
+   category_id = Column(Integer)
+   tournament_id = Column(Integer)
+   value= Column(String)
+
+class Config(Base):
+   __tablename__ = 'config'
+   
+   key = Column(String, primary_key = True)
+   value= Column(String)
 
 #Index('myindex', Log.bot, Log.market, Log.time)
 
@@ -44,3 +62,48 @@ def Session():
       finally:
           session.close()
   return session
+
+def get_config_be(session, key):
+      try:
+          config = session.query(Config).filter_by(key=key).one()
+      except NoResultFound as e:
+          config = Config(key=key)
+          session.add(config)
+      return config
+
+def update_config(key, value):
+    with Session()() as session:
+        config = get_config_be(session, key)
+        config.value = json.dumps(value)
+
+def get_config(key):
+    with Session()() as session:
+        config = get_config_be(session, key)
+        if config.value:
+            return json.loads(config.value)
+        return None
+
+        
+
+def update_match(match):
+    match_id = match["matchId"]
+    with Session()() as session:
+        try:
+            db_match = session.query(Match).filter_by(match_id=match_id).one()
+        except NoResultFound as e:
+            db_match = Match(match_id=match_id,
+              sport_id=match["sportId"],
+              category_id=match["categoryId"],
+              tournament_id=match["tournamentId"],
+              )
+            session.add(db_match)
+        db_match.value = json.dumps(match)
+
+def historize_outcome(outcome):
+    time = datetime.now().timestamp()
+    with Session()() as session:
+      history = History(
+        outcome_id=outcome["outcomeId"],
+        time=time,
+        data=json.dumps(outcome))
+      session.add(history)
--- winamax/winamax.py
+++ winamax/winamax.py
@@ -6,110 +6,95 @@ from datetime import datetime
 from http.cookiejar import LWPCookieJar
 import copy
 from . import db
+from .http import Http
+import time
+
 
 class Winamax():
     def __init__(self):
         self._data = None
         self.Session = db.Session()
 
-    def extract(self, text):
-        p= re.compile('var PRELOADED_STATE = (\{((?!\<script).)*});')
-        m = p.search(text)
-        res = json.loads(m.group(1))
-        return res
+    def update_sports(self):
+        http = Http()
+        res_sports = []
+        res_tournaments = []
+        for sport_id in http.data["sports"]:
+            sport = http.get("sports", sport_id)
+            res_sport = { "name": sport["sportName"], "categories": []}
+            for category_id in sport["categories"]:
+                category = http.get("categories", category_id)
+                res_category = {"name": category["categoryName"], "tournaments": []}
+                res_sport["categories"].append(res_category)
+                for tournament_id in category["tournaments"]:
+                    tournament = http.get("tournaments", tournament_id)
+                    res_tournament = {"name": tournament["tournamentName"],
+                    "suffix": f"{sport_id}/{category_id}/{tournament_id}"}
+                    res_category["tournaments"].append(res_tournament)
+                    res_tournaments.append(res_tournament)
+            res_sports.append(res_sport)
+            
 
-    def get_remote_data(self):
-        url = f"https://www.winamax.fr/paris-sportifs/sports/"
-        response = requests.get(url)
-        return self.extract(response.text)
+        db.update_config("sports", res_sports)
+        db.update_config("tournaments", res_tournaments)
 
-    def get_cache(self):
-        try:
-            with open(f'./cache.json', 'r') as infile:
-                return json.load(infile)
-        except:
-            self.update_cache()
-            return self.get_cache()
-
-    def update_cache(self):
-        data = self.get_remote_data()
-        with open(f'./cache.json', 'w') as outfile:
-            json.dump(data, outfile, indent=4)
-            self._data = None
-
-    @property
-    def data(self):
-        if not self._data:
-            self._data = self.get_cache()
-        return self._data
-
-
-    def get_thing(self, type, type_id):
-        type_id = str(type_id)
-        thing = self.data[f"{type}"].get(type_id)
-        return copy.deepcopy(thing)
-
-    def get_sport(self, sport_id):
-        sport = self.get_thing("sports", sport_id)
-        if sport and "categories" in sport:
-            sport["sportId"] = sport_id
-            del sport["categories"]
-            del sport["filters"]
-        return sport
-
-    def get_outcome(self, outcome_id):
-        outcome = self.get_thing("outcomes", outcome_id)
-        if outcome:
-            outcome["outcomeId"] = outcome_id
-        return outcome
-
-    def get_bet(self, bet_id):
-        bet = self.get_thing("bets", bet_id)
-        """
-        if bet:
-            outcomes = {}
-            for i in range(len(bet["outcomes"])):
-                outcome_id = bet["outcomes"][i]
-                bet["outcomes"][i] = self.get_outcome(outcome_id)
-                if bet["outcomes"][i]:
-                    bet["outcomes"][i]["outcomeId"] = outcome_id
-                    bet["outcomes"][i]["odds"] = self.get_thing("odds", outcome_id)
-        """
-
-        return bet
+    def update_next_tournament(self):
+        last_updated_tournament = db.get_config("last_updated_tournament")
+        tournaments = db.get_config("tournaments")
+        if last_updated_tournament == None:
+            last_updated_tournament = -1
+        else:
+            last_updated_tournament = last_updated_tournament["value"]
+        last_updated_tournament = last_updated_tournament + 1
+        if last_updated_tournament >= len(tournaments):
+            last_updated_tournament = 0
+        suffix = tournaments[last_updated_tournament]["suffix"]
+        print(suffix)
+        self.update_tournament(suffix)
+        db.update_config("last_updated_tournament", { "value": last_updated_tournament})
+        
 
 
-    def get_match(self, match_id):
-        match = self.get_thing("matches", match_id)
-        if match:
-            match["bet"] = self.get_bet(match["mainBetId"])
-            match["sport"] = self.get_sport(match["sportId"])
-            del match["filters"]
-        return match
-
-    def get_matches(self, sport_id=None):
-        res = []
-        for key, match in self.data["matches"].items():
-            if not sport_id or sport_id == match["sportId"]:
-                res.append(self.get_match(match["matchId"]))
-        return res
+        
+    def update_tournament(self, suffix):
+        http = Http(suffix)
+        for match_id in http.data["matches"]:
+            match = http.data["matches"][match_id]
+            bet = http.get("bets", match["mainBetId"])
+            match["bet"] = bet
+            db.update_match(match)
+            for outcome_id in bet["outcomes"]:
+                outcome = http.get("outcomes", outcome_id)
+                outcome["outcomeId"] = outcome_id
+                db.historize_outcome(outcome)
 
 
-    def get_sports(self):
-        res = []
-        for sport_id in self.data["sportIds"]:
-            res.append(self.get_sport(sport_id))
-        return res
 
-    def get_outcomes(self):
-        res = []
-        for outcome_id, outcome in self.data["outcomes"].items():
-            outcome = self.get_outcome(outcome_id)
-            if outcome:
-                outcome["odds"] = self.get_thing("odds", outcome_id)
-                res.append(outcome)
-        return res
+        
+    """
+    def update_sport_cache(self, cache, sport_id):
+        cache = Cache(f"/{sport_id}")
+        print(json.dumps(cache.data["categories"], indent=4))
+        for category_id in cache.data["categories"]:
+            break
 
+    def update_cache(self, suffix):
+        print(f"Updating cache {suffix}")
+        local_cache = Cache(suffix)
+        local_cache.update_cache()
+        time.sleep(5)
+
+    def update_all_caches(self):
+        suffix = ""
+        cache = Cache(suffix)
+        for sport_id, sport in cache.data["sports"].items():
+            self.update_cache(f"/{sport_id}")
+            for category_id in sport["categories"]:
+                category = cache.get_category(category_id)
+                self.update_cache(f"/{sport_id}/{category_id}")
+                for tournament_id in category["tournaments"]:
+                    self.update_cache(f"/{sport_id}/{category_id}/{tournament_id}")
+                    
 
     def take_outcomes_snapshot(self):
         self.update_cache()
@@ -145,3 +130,4 @@ class Winamax():
 
     def serialize_all(self, history):
         return [ self.serialize(h) for h in history ]
+    """
